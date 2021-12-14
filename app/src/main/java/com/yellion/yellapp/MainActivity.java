b760c874efde6478ad788a917d8a6f9e64824526
Cong Thanh Vu <congthanh3181@gmail.com>
2021-12-14 23:42:58 +0700

    Sign in + Sign up

--- app/src/main/java/com/yellion/yellapp/MainActivity.java
+++ app/src/main/java/com/yellion/yellapp/MainActivity.java
@@ -26,8 +26,6 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         setNavListener();
     }
