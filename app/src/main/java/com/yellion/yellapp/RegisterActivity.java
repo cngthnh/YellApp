0a6f88bd82faabf5cd694db38c4b619a34b7b650
Cong Thanh Vu <congthanh3181@gmail.com>
2021-12-23 15:30:55 +0700

    Password reveal, fixed logout, save current registration, added resend verification email

--- app/src/main/java/com/yellion/yellapp/RegisterActivity.java
+++ app/src/main/java/com/yellion/yellapp/RegisterActivity.java
@@ -11,10 +11,14 @@ import android.text.Html;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.TextWatcher;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
@@ -88,6 +92,17 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         editor.apply();
     }
 
+    private void removePrevInfo() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        currentEmail = null;
+        currentUsername = null;
+        currentHash = null;
+        editor.remove("CR_UID");
+        editor.remove("CR_HSH");
+        editor.remove("CR_EML");
+        editor.apply();
+    }
+
     private void setupInputRules() {
         registerValidator.addValidation(binding.nameInput, RegexTemplate.NOT_EMPTY, getResources().getString(R.string.invalid_name));
         registerValidator.addValidation(binding.usernameInput, RegexTemplate.NOT_EMPTY, getResources().getString(R.string.invalid_username));
@@ -101,6 +116,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     }
 
     private void prepareComponents() {
+        binding.revealPassword.setOnClickListener(this);
+        binding.cancelVeri.setOnClickListener(this);
         binding.resend.setOnClickListener(this);
         binding.checkVeri.setOnClickListener(this);
         binding.registerBtn.setOnClickListener(this);
@@ -218,10 +235,51 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             resendEmail();
         } else if (viewId == R.id.checkVeri) {
             login(true);
+        } else if (viewId == R.id.cancelVeri) {
+            removePrevInfo();
+            showForm();
+        } else if (viewId == R.id.revealPassword) {
+            revealOrHidePassword(view);
+        }
+    }
+
+    private void revealOrHidePassword(View view) {
+        ImageButton imageButton = (ImageButton) view;
+        if (binding.passwordInput.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
+            binding.passwordInput.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            imageButton.setImageResource(R.drawable.ic_eye_hide);
+        } else {
+            binding.passwordInput.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            imageButton.setImageResource(R.drawable.ic_eye_show);
         }
     }
 
     private void resendEmail() {
+        showLoading();
+
+        UserCredentials userCredentials = new UserCredentials(currentUsername);
+
+        String jsonCredentials = moshi.adapter(UserCredentials.class).toJson(userCredentials);
+
+        RequestBody requestBody = RequestBody.create(MediaType.parse("text/plain"), jsonCredentials);
+
+        Call<InfoMessage> resendCall;
+        resendCall = service.resendVerification(requestBody);
+        resendCall.enqueue(new Callback<InfoMessage>() {
+            @Override
+            public void onResponse(Call<InfoMessage> call, Response<InfoMessage> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(RegisterActivity.this, "Đã gửi lại email xác thực", Toast.LENGTH_LONG).show();
+                    showConfForm();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<InfoMessage> call, Throwable t) {
+                Toast.makeText(RegisterActivity.this, "Không thể gửi email xác thực", Toast.LENGTH_LONG).show();
+                showConfForm();
+            }
+        });
     }
 
     private void login(boolean stay) {
@@ -243,6 +301,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
                 if (response.isSuccessful()) {
                     sessionManager.saveToken(response.body());
+                    removePrevInfo();
                     startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                     finish();
                 } else {
@@ -251,6 +310,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                         showConfForm();
                     }
                     else {
+                        removePrevInfo();
                         gotoLogin();
                         Toast.makeText(RegisterActivity.this, "Email chưa được xác thực. Vui lòng thử đăng nhập lại", Toast.LENGTH_LONG).show();
                     }
@@ -376,6 +436,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        savePrevInfo();
+        if (currentUsername != null)
+            savePrevInfo();
     }
 }
