bcde552badc90c17958873603870642144b449b7
Ngo Truong Huy <truonghuycorn@gmail.com>
2022-01-19 15:08:46 +0700

    - Config API for Task (continue v2)

--- app/src/main/java/com/yellion/yellapp/TaskFragment.java
+++ app/src/main/java/com/yellion/yellapp/TaskFragment.java
@@ -3,20 +3,26 @@ package com.yellion.yellapp;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
@@ -29,6 +35,9 @@ import com.yellion.yellapp.models.YellTask;
 import com.yellion.yellapp.utils.ApiService;
 import com.yellion.yellapp.utils.Client;
 import com.yellion.yellapp.utils.SessionManager;
+
+import java.text.ParseException;
+import java.util.Date;
 
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
@@ -45,19 +54,17 @@ public class TaskFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM1 = "taskId";
+    private static final String ARG_PARAM2 = "dashboardId";
     SessionManager sessionManager;
     ApiService service;
-    String taskId;
-    String dashBoardId;
     Moshi moshi = new Moshi.Builder().build();
     YellTask currentYellTask;
     private FragmentTaskBinding binding;
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String taskId;
+    private String dashBoardId;
 
     public TaskFragment() {
         // Required empty public constructor
@@ -67,16 +74,16 @@ public class TaskFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param taskId Parameter 1.
+     * @param dashBoardId Parameter 2.
      * @return A new instance of fragment TaskFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TaskFragment newInstance(String param1, String param2) {
+    public static TaskFragment newInstance(String taskId, String dashBoardId) {
         TaskFragment fragment = new TaskFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM1, taskId);
+        args.putString(ARG_PARAM2, dashBoardId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -85,8 +92,9 @@ public class TaskFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            taskId = getArguments().getString(ARG_PARAM1);
+            dashBoardId = getArguments().getString(ARG_PARAM2);
+            currentYellTask = new YellTask(dashBoardId,taskId);
         }
     }
 
@@ -126,6 +134,9 @@ public class TaskFragment extends Fragment {
                 if ((String)taskIcon.getTag() == "false") {
                     taskName.setEnabled(true);
                     taskName.requestFocusFromTouch();
+                    InputMethodManager imm = (InputMethodManager)
+                            getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(taskName, InputMethodManager.SHOW_IMPLICIT);
                     taskName.setSelection(taskName.getText().length());
 
                     taskIcon.setClickable(true);
@@ -151,6 +162,8 @@ public class TaskFragment extends Fragment {
 
                     deleteTask.setImageResource(R.drawable.ic_delete);
                     deleteTask.setColorFilter(null);
+
+                    currentYellTask.setName(taskName.getText().toString());
                 }
             }
         });
@@ -212,7 +225,31 @@ public class TaskFragment extends Fragment {
                         "DeadlineTimeDialog");
             }
         });
+        deadlineTask.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void afterTextChanged(Editable s) {
+                String temp = s.toString();
+                SimpleDateFormat currentFormat = new SimpleDateFormat("HH:mm  dd/MM/YYYY");
+                SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                try {
+                    Date date = currentFormat.parse(temp);
+                    currentYellTask.setEnd_time(isoFormat.format(date));
+                } catch (ParseException e) {
+                    Log.e("TimeParseError","Time Parse Error");
+                }
+            }
+        });
         AppCompatTextView priority = binding.priorityTextView;
         priority.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -227,6 +264,30 @@ public class TaskFragment extends Fragment {
                             }
                         });
                 priorityDialog.show();
+            }
+        });
+        priority.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String temp = s.toString();
+                Integer value;
+                if (temp == "Cao")
+                    value = 2;
+                else if (temp == "Thường")
+                    value = 1;
+                else
+                    value = 0;
+                currentYellTask.setPriority(value);
             }
         });
 
@@ -244,6 +305,28 @@ public class TaskFragment extends Fragment {
                             }
                         });
                 statusDialog.show();
+            }
+        });
+        status.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String temp = s.toString();
+                Integer value;
+                if (temp == "Đã hoàn thành")
+                    value = 1;
+                else
+                    value = 0;
+                currentYellTask.setStatus(value);
             }
         });
 
@@ -276,6 +359,8 @@ public class TaskFragment extends Fragment {
                     editContent.setImageResource(R.drawable.ic_edit_mode);
 
                     editContentDiscard.setVisibility(View.GONE);
+
+                    currentYellTask.setContent(content.getText().toString());
                 }
             }
         });
